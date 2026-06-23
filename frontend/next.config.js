da30c908739c992a538b94790091fc88ca0db04c
thevintijeffect <thevintijeffect@gmail.com>
2026-06-24 02:24:28 +0800

    Add custom headers configuration to Next.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Cache-Control", value: "no-store, max-age=0, must-revalidate" },
+          { key: "Content-Type", value: "text/html; charset=utf-8" },
+        ],
+      },
+    ];
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
