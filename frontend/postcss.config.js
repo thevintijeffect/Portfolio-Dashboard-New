3be8be948f531eb2923ef833a16ef7f6ce9ba041
thevintijeffect <thevintijeffect@gmail.com>
2026-06-24 00:43:32 +0800

    Configure PostCSS with Tailwind CSS and Autoprefixer

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-module.exports = nextConfig;
