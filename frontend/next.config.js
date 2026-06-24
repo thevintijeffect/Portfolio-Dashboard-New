ce9ff8c114df9357633893f3bfe6f6a00d221ab4
thevintijeffect <thevintijeffect@gmail.com>
2026-06-24 09:57:34 +0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
         headers: [
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Cache-Control", value: "no-store, max-age=0, must-revalidate" },
-          { key: "Content-Type", value: "text/html; charset=utf-8" },
         ],
       },
     ];
